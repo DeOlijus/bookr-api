c23540a6f73c17af0041adf1ca5e9d0ac1d76958
justin DeOliveira <deoli.justin@gmail.com>
2024-08-08 00:08:00 -0400

    Basic Installation and Configuration for Validation and Logging

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { DatabaseModule } from '@app/common';
+import { ReservationRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationRepository],
+})
+export class ReservationsModule {}
